87a705bfaf30d3e6c1ec4c4b3d0cd65429aae22a
bestkittathat <jarbestx123@gmail.com>
2022-08-15 23:30:00 +0700

    add func 20 button

--- app/src/main/java/com/example/fruitvegcamera/Showdatafruit.java
+++ app/src/main/java/com/example/fruitvegcamera/Showdatafruit.java
@@ -1,14 +1,164 @@
 package com.example.fruitvegcamera;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Showdatafruit extends AppCompatActivity {
 
+    ImageButton Watermelon,tomato,Strawberry,Rambutan,Papaya,Mango,Pumpkin,
+            Potato,Pineapple,Orange,Grapes,Cucumber,WaterSpinach,Coconut,ChineseCabbage,
+            Carrot,Cabbage,Banana,Broccoli,Apple;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_showdatafruit);
+
+        Watermelon = findViewById(R.id.Watermelon);
+        tomato = findViewById(R.id.tomato);
+        Strawberry = findViewById(R.id.Strawberry);
+        Rambutan = findViewById(R.id.Rambutan);
+        Papaya = findViewById(R.id.Papaya);
+        Mango = findViewById(R.id.Mango);
+        Pumpkin = findViewById(R.id.Pumpkin);
+        Potato = findViewById(R.id.Potato);
+        Pineapple = findViewById(R.id.Pineapple);
+        Orange = findViewById(R.id.Orange);
+        Grapes = findViewById(R.id.Grapes);
+        Cucumber = findViewById(R.id.Cucumber);
+        WaterSpinach = findViewById(R.id.WaterSpinach);
+        Coconut = findViewById(R.id.Coconut);
+        ChineseCabbage = findViewById(R.id.ChineseCabbage);
+        Carrot = findViewById(R.id.Carrot);
+        Cabbage = findViewById(R.id.Cabbage);
+        Banana =  findViewById(R.id.Banana);
+        Broccoli = findViewById(R.id.Broccoli);
+        Apple = findViewById(R.id.Apple);
+
+
+        Watermelon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        tomato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Strawberry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Rambutan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Papaya.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Mango.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Pumpkin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Potato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Pineapple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+       Orange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Grapes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Cucumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        WaterSpinach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Coconut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        ChineseCabbage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Carrot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Cabbage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Banana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+       Broccoli.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Apple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
